82c265f4d7c672aa97ec57b2e6cf2e2c9d6a054b
Szymon Solak <16944591+szymon-solak@users.noreply.github.com>
2023-07-11 21:44:12 +0200

    feat: Add OrcaSlicer

--- home-manager/modules/3dp.nix
+++ home-manager/modules/3dp.nix
@@ -1,25 +1,20 @@
 { pkgs, ... }:
 let
-  orcaSlicer = pkgs.appimageTools.wrapType2 {
-    name = "OrcaSlicer";
-    # Having some issues with zip, maybe proxy to AppImage directly? (direct download seems fine..)
-    src = pkgs.fetchzip {
-      url =
-        "https://github.com/SoftFever/OrcaSlicer/releases/download/v1.6.3/OrcaSlicer_V1.6.3_Linux.zip";
-      # url = "https://github.com/bambulab/BambuStudio/releases/download/v01.06.02.04/Bambu_Studio_linux_ubuntu_v01.06.02.04-20230427094209.AppImage"; # Bambu for testing appimage directly
-      # hash = "sha256-J5lwGmRj2qU2E2xgbL40sIN/IinSIqt9PcViP9yiRjc=";
-      hash = "sha256-n/D0qfli+kPPKgDIujsPdZHdoUPex3SFuSqLlhGzxJY=";
-      # hash = "sha256-8daajQTWROZ2q4ZPU3ooJHz1Aar99aLj/2y9a4gvMKE=";
-      # downloadToTemp = true;
-      # postFetch = ''
-      # 	${pkgs.unzip}/bin/unzip -p $downloadedFile OrcaSlicer_ubu64.AppImage > $out
-      # '';
-    };
-    extraPkgs = pkgs: with pkgs; [ webkitgtk gcc12 ];
+  orcaslicer = pkgs.appimageTools.wrapType2 rec {
+		name = "OrcaSlicer";
+    pname = "orcaslicer";
+		version = "1.6.3";
+		src =
+			let extracted = pkgs.fetchzip {
+				url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_V1.6.3_Linux.zip";
+				sha256 = "sha256-n/D0qfli+kPPKgDIujsPdZHdoUPex3SFuSqLlhGzxJY=";
+			};
+			in "${extracted}/OrcaSlicer_ubu64.AppImage";
+    extraPkgs = pkgs: with pkgs; [ webkitgtk ];
   };
 in {
   home.packages = [
-    # orcaSlicerpkgs
     pkgs.prusa-slicer
+    orcaslicer
   ];
 }
