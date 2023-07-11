95880263defb9498c10d701258ac19d43d25f0ae
Szymon Solak <16944591+szymon-solak@users.noreply.github.com>
2023-07-11 20:16:17 +0200

    feat: Add node20 shell

---
+++ shells/node20/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Node 20.x JS devShell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "node-env";
+
+          buildInputs = [
+            pkgs.nodejs_20
+            pkgs.nodePackages.jsonlint
+            pkgs.nodePackages.eslint
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+            # pkgs.nodePackages.vscode-langservers-extracted
+          ];
+        };
+      });
+}
