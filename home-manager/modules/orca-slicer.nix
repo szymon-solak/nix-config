a145bf46263999e899be95031e46c0ec87393ed2
Szymon Solak <16944591+szymon-solak@users.noreply.github.com>
2024-12-20 23:19:29 +0100

    feat: Fix orca-slicer dependencies

--- home-manager/modules/orca-slicer.nix
+++ home-manager/modules/orca-slicer.nix
@@ -7,7 +7,7 @@
       url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/OrcaSlicer_Linux_V${version}.AppImage";
       sha256 = "sha256-3uqA3PXTrrOE0l8ziRAtmQ07gBFB+1Zx3S6JhmOPrZ8=";
     };
-    extraPkgs = pkgs: with pkgs; [webkitgtk_4_1];
+    extraPkgs = pkgs: with pkgs; [webkitgtk_4_0];
   };
 in {
   home.packages = [
